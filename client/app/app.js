4e062b76a63b80f8b489bf19c172f36426c6b0f1
Patrick Lynch <patrickflynch@gmail.com>
2016-03-28 21:42:29 -0500

    random drink on page load, smaller on mobile

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,11 @@
-angular.module('drinkRec', []);
+var app = angular.module('drinkRec', []);
+
+app.controller('drinkRecCtrl', function($scope, $http) {
+
+  $http.get('../data/recipes.json')
+    .then(function(res){
+      $scope.drinkList = res.data;
+      var rand = Math.floor((Math.random() * $scope.drinkList.length) + 1);
+      $scope.selectedDrink = $scope.drinkList[rand];
+    });
+});
